e6117c9664fcd6a0451aa40c9ecd7721cb1d3cb0
Lisa Olson <olson.lisa94@gmail.com>
2018-04-11 06:05:58 +0000

    removes unnecessary conditional logic

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 for i in range(100):
-    if i < 10:
-        print("0{:d}".format(i), end=", ")
-    elif i >= 10 and i < 99:
-        print(i, end=", ")
-    elif i == 99:
+    if i == 99:
         print("{:d}".format(i))
+    print("{:02}".format(i), end=", ")
