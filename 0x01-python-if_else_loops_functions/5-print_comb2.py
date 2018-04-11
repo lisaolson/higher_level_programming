61f9f7eaf765f478f92de4109fefc24997262560
Lisa Olson <olson.lisa94@gmail.com>
2018-04-11 05:58:33 +0000

    updates formatting and adds double quotes

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 for i in range(100):
     if i < 10:
-        print("0{:d}".format(i), end=', ')
+        print("0{:d}".format(i), end=", ")
     elif i >= 10 and i < 99:
-        print(i, end=', ')
+        print(i, end=", ")
     elif i == 99:
-        print(i)
+        print("{:d}".format(i))
