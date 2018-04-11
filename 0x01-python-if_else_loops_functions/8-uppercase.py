e47100141fdea345ce821c6976d2f29fc416b88e
Lisa Olson <olson.lisa94@gmail.com>
2018-04-11 05:21:27 +0000

    pep8 compliance

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,5 @@ def uppercase(str):
     for i in str:
         if i >= 'a' and i <= 'z':
             i = chr(ord(i) - 32)
-        print("{}".format(i), end = '')
+        print("{}".format(i), end='')
     print('')
