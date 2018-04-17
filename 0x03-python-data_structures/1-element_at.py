ad187a56194d5f6de78fa8ede5c41e470f4f7e44
Lisa Olson <olson.lisa94@gmail.com>
2018-04-17 22:11:39 +0000

    remove main spacing

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def element_at(my_list, idx):
+def element_at(my_list, idx):
         if idx > 0 and idx <= len(my_list):
             return my_list[idx]
         elif idx > len(my_list):
