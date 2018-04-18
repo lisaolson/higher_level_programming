02aa5ce80ff161f36de4c12b25590db2666e7106
Lisa Olson <olson.lisa94@gmail.com>
2018-04-18 18:14:09 +0000

    changes if condition to check for end of row

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for x in row:
-            if x % 3 == 0:
-                print("{:d}".format(x), end="\n")
-            else:
-                print("{:d}".format(x), end=" ")
-
+    if matrix:
+        for row in matrix:
+            for x in row:
+                if x != row[-1]:
+                    print("{:d}".format(x), end=" ")
+                else:
+                    print("{:d}".format(x), end="")
+            print("")
