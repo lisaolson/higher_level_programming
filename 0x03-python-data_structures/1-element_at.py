bef70caccc1d13419625cb9b3f2d0728ea0c78fe
Lisa Olson <olson.lisa94@gmail.com>
2018-04-18 02:56:24 +0000

    removes extra conditional

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx > 0 and idx < len(my_list):
+    if not my_list:
+        return None
+
+    if idx < len(my_list):
         return my_list[idx]
     elif idx >= len(my_list):
         return None
