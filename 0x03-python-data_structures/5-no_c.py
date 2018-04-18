b67b99d2823d335f33b5da993881402287f01ffc
Lisa Olson <olson.lisa94@gmail.com>
2018-04-18 04:42:37 +0000

    refactors code to iterate through string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,24 +1,8 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def no_c(my_string):
-        mod_string = my_string
-        ret1 = my_string.find('c')
-        ret2 = my_string.find('C')
-
-        if ret2 > 0:
-            mod_string = my_string[:ret2] + my_string[(ret2 + 1):]
-            return mod_string
-        if ret1 > 0 or ret2 > 0:
-            mod_string = my_string[:ret1] + my_string[(ret1 + 1):]
-            mod_string = my_string[:ret2] + my_string[(ret2 + 1):]
-            return mod_string
-        else:
-            print("broken")
-#    elif ret2 > 0:
- #       my_string = my_string[:ret1] + my_string[(ret1 + 1):]
-  #      return my_string
-  #  if ret2 > 0:
-   #     my_string = my_string[:ret2] + my_string[(ret2 + 1):]
-    #    return my_string
-   # else:
-    #    print("Woah")
+def no_c(my_string):
+    new_str = []
+    for x in my_string:
+        if x != 'c' and x != 'C':
+            new_str.append(x)
+    new_str = "".join(new_str)
+    return new_str
