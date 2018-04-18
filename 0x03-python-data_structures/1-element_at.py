1e704cde05f9deaaf2089658a11f4d61dbdc80d4
Lisa Olson <olson.lisa94@gmail.com>
2018-04-18 02:57:57 +0000

    returns nothing

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if my_list == []:
-        return None
+    if not my_list:
+        return
 
     if idx < len(my_list):
         return my_list[idx]
