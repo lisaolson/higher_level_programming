b83a3914f6a36e10a7e8efc374a909eb7cb0a463
Lisa Olson <olson.lisa94@gmail.com>
2018-04-18 03:03:24 +0000

    adds greater than or equal to len

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,7 +3,7 @@ def replace_in_list(my_list, idx, element):
     if idx < len(my_list) and idx >= 0:
         my_list[idx] = element
         return my_list
-    elif idx > len(my_list):
+    elif idx >= len(my_list):
         return None
     else:
         return None
