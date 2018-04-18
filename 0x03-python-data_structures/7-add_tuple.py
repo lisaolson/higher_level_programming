c9ebfed88ebdaf7fe6ea1824ddc0acc2cdb4167d
Lisa Olson <olson.lisa94@gmail.com>
2018-04-18 21:12:31 +0000

    changes return values if argument entered is empty

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) == 1:
-        total = ((tuple_a[0] + tuple_b[0]), (0 + tuple_b[1]))
+        total = (tuple_a + (0))
         return total
     if len(tuple_b) == 1:
-        total = ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + 0))
+        total = (tuple_b, + (0))
         return total
     if len(tuple_a) == 0:
         tuple_a = (0, 0)
