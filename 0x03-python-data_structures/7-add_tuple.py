0649a061c3a7a32983b1e0e44857de222ea21a07
Lisa Olson <olson.lisa94@gmail.com>
2018-04-18 17:17:23 +0000

    adds check for tuple_a and tuple_b len being 0

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -6,10 +6,9 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_b) == 1:
         total = ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + 0))
         return total
-    if len(tuple_a) == 0 or len(tuple_b) == 0:
-        tuple_a = 1
-        tuple_b = 89
-        total = (tuple_a, tuple_b)
-        return total
+    if len(tuple_a) == 0:
+        tuple_a = (0, 0)
+    if len(tuple_b) == 0:
+        tuple_b = (0, 0)
     total = ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
     return total
