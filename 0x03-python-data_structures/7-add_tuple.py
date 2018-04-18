2e5955ad290bd9e61d88f744ce07907e2237be0c
Lisa Olson <olson.lisa94@gmail.com>
2018-04-18 21:45:48 +0000

    replaces return total values with tuple_a and tuple_b values

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) == 1:
-        total = (tuple_a + (0))
-        return total
+        tuple_a = (tuple_a[0], 0)
     if len(tuple_b) == 1:
-        total = (tuple_b, + (0))
-        return total
+        tuple_b = (tuple_b[0], 0)
     if len(tuple_a) == 0:
         tuple_a = (0, 0)
     if len(tuple_b) == 0:
