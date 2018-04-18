7e46266a0a05fc163ac87bee0a759ac0c89bfad9
Lisa Olson <olson.lisa94@gmail.com>
2018-04-18 21:06:16 +0000

    removes null check and replaces [] with x

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if not my_list:
-        return
-
     new_list = []
     for x in my_list:
-        if my_list[x] % 2 == 0:
+        if x % 2 == 0:
             new_list.append(True)
         else:
             new_list.append(False)
