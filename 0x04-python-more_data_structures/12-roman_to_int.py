e339be457bc6e530d482f0eaed07e9a0dac31db9
Lisa Olson <olson.lisa94@gmail.com>
2018-04-23 04:33:59 +0000

    pep8

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
-    numbers = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+    numbers = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 
     prev = 0
     total = 0
@@ -12,4 +12,3 @@ def roman_to_int(roman_string):
         total += numbers[k]
         prev = numbers[k]
     return total
-
