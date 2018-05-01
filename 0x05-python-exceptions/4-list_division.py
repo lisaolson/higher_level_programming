e230c5417b57b8e669b933150d5d969650ee59b8
Lisa Olson <olson.lisa94@gmail.com>
2018-05-01 03:27:38 +0000

    updates value of total at error checks

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -8,10 +8,13 @@ def list_division(my_list_1, my_list_2, list_length):
             total = my_list_1[x] / my_list_2[x]
         except ZeroDivisionError:
             print("division by 0")
+            total = 0
         except TypeError:
             print("wrong type")
+            total = 0
         except IndexError:
             print("out of range")
+            total = 0
         finally:
             new_list.append(total)
     return new_list
