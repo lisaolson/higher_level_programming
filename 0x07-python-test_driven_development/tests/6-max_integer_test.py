f8e49cab53ab15b0113af66312e59c56adac995a
Lisa Olson <olson.lisa94@gmail.com>
2018-05-06 01:20:15 +0000

    accounts for various data types

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -10,8 +10,13 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([1, 3, 4, 2]), 4)
         self.assertEqual(max_integer([-5, 4, 10]), 10)
         self.assertEqual(max_integer([-15, -12]), -12)
+        self.assertEqual(max_integer([4.1, 4, 2, 3]), 4.1)
+        self.assertEqual(max_integer([5729810298347362832973, 2345, 1234]), 5729810298347362832973)
+        self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer([True, False, 200]), 200)
 
         self.assertRaises(TypeError, max_integer, [1, 2, "a"])
+        self.assertRaises(TypeError, max_integer, (1,2,3,4))
 
 if __name__ == "__main__":
     unittest.main()
