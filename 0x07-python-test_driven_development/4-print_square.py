579a80541525cdf2d5be6473ced4d6a0febafc60
Lisa Olson <olson.lisa94@gmail.com>
2018-05-09 21:23:45 +0000

    rearranges if statement within for loop

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -20,9 +20,11 @@ def print_square(size):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    if type(size) is float and size < 0:
-        raise TypeError("size must be an integer")
+#    if type(size) is float and size < 0:
+ #       raise TypeError("size must be an integer")
     for x in range(0, size):
         for y in range(0, size):
-            print('#', end="")
-        print()
+            if y != (size - 1):
+                print('#', end="")
+            else:
+                print('#')
