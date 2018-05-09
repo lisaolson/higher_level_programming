bcd81c85fe0cbc07ed314de88334179f345d3a09
Lisa Olson <olson.lisa94@gmail.com>
2018-05-09 21:26:01 +0000

    replaces if statement before loops

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -20,8 +20,8 @@ def print_square(size):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-#    if type(size) is float and size < 0:
- #       raise TypeError("size must be an integer")
+    if type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
     for x in range(0, size):
         for y in range(0, size):
             if y != (size - 1):
