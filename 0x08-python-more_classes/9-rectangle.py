b9710562a5e47998583cee727d896eb9570600cb
Lisa Olson <olson.lisa94@gmail.com>
2018-05-09 23:16:49 +0000

    removes comments

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -171,10 +171,6 @@ class Rectangle:
         Returns:
             new width and height for Rectangle instance
         """
-#        if type(size) is not int:
- #           raise TypeError("width must be an integer")
-  #      if size < 0:
-   #         raise ValueError("width must be >= 0")
         width = size
         height = size
         return cls(width, height)
