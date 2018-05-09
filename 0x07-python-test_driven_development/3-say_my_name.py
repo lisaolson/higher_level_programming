034e0d29156fd05fca39085616d17ef07f3c3911
Lisa Olson <olson.lisa94@gmail.com>
2018-05-09 21:06:58 +0000

    Removes cases for none

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -16,11 +16,7 @@ def say_my_name(first_name, last_name=""):
         None
     """
 
-    if first_name is None:
-        raise TypeError("first_name must be a string")
-    if last_name is None:
-        raise TypeError("last_name must be a string")
-    if type(first_name) is not str:
+   if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
