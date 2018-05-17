98f4ae961fa365ab533ec34044c313af7f701e39
Lisa Olson <olson.lisa94@gmail.com>
2018-05-17 02:02:54 +0000

    removes extra newlines

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -8,10 +8,10 @@ def read_lines(filename="", nb_lines=0):
             lineNum += 1
         if nb_lines >= lineNum or nb_lines <= 0:
             f.seek(0, 0)
-            print(f.read())
+            print(f.read(), end="")
         else:
             lineNum = 0
             f.seek(0, 0)
             while (lineNum < nb_lines):
-                print(f.readline())
+                print(f.readline(), end="")
                 lineNum += 1
