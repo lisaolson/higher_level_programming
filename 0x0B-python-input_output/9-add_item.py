079a414800c8f6203e3585f2ce0869c1980defa9
Lisa Olson <olson.lisa94@gmail.com>
2018-05-18 04:57:57 +0000

    adds all arguments to a python list

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -6,12 +6,12 @@ import sys
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
-my_list = [1, 2, 3]
-save_to_json_file(my_list, filename)
-print(my_list)
+try:
+    list_args = load_from_json_file("add_item.json")
+except Exception:
+    list_args = []
 
-i = 0
-while (sys.argv != '\0'):
-    print(sys.argv)
-    i += 1
+for i in sys.argv[1:]:
+    list_args.append(i)
+
+save_to_json_file(list_args, "add_item.json")
