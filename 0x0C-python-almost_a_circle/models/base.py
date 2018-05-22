400850b47e0edf27c4f361c9f2701ef5fa273e0e
Lisa Olson <olson.lisa94@gmail.com>
2018-05-22 23:30:39 +0000

    adds create and json string to dictionary method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
+import json
+
+#from models.rectangle import Rectangle
+#from models.square import Square
+
 
 class Base:
     __nb_objects = 0
@@ -10,3 +15,42 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None:
+            return "[]"
+        if not list_dictionaries:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        filename = cls.__name__ + ".json"
+        json_list_objs = Base.to_json_string([obj.to_dictionary() for obj in list_objs])
+        with open(filename, "w") as f:
+            f.write(json_list_objs) 
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None or not json_string:
+            return []
+        else:
+            return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        from models.rectangle import Rectangle
+        from models.square import Square
+        if cls.__name__ == "Rectangle":
+            new = cls(1, 1)
+        if cls.__name__ == "Square":
+            new = cls(1)
+        new.update(**dictionary)
+        return new
+            
+    @classmethod
+    def load_from_file(cls):
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as f:
+            f.write()
