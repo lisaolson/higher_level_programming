c1363d1029a2062d81e9bd71646bcecc7f43a7fd
Lisa Olson <olson.lisa94@gmail.com>
2018-05-23 06:00:50 +0000

    tests framework

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Unittest for Base class
+"""
+
+import unittest
+
+from models.rectangle import Rectangle
+from models.square import Square
+from models import Rectangle
+from models import Square
+
+class TestBase(unittest.TestCase):
+    
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Unittest for Base class
+"""
+
+import unittest
+
+from models.rectangle import Rectangle
+from models.square import Square
+
+
+class TestSquare(unittest.TestCase):
+
+
+if __name__ == "__main__":
+    unittest.main()
