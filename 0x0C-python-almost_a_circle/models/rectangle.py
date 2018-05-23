204d7915219d1f9e8ada3f113c3be744abf2f8f1
Lisa Olson <olson.lisa94@gmail.com>
2018-05-23 05:18:02 +0000

    pep8 error line space, remove private

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -121,9 +121,8 @@ class Rectangle(Base):
 
     def __str__(self):
         """Returns formatted string"""
-
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
-               self.__x, self.__y, self.__width, self.__height)
+        strr = "[Rectangle] ({}) {}/{} - {}/{}"
+        return strr.format(self.id, self.x, self.y, self.width, self.height)
 
     def update(self, *args, **kwargs):
         """Assigns an argument to each attribute
