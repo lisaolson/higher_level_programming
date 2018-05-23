1397be344aa03649b91d79d1f17ee0dd5a28b681
Lisa Olson <olson.lisa94@gmail.com>
2018-05-23 06:09:56 +0000

    changes to else logic

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -62,9 +62,10 @@ class Base:
         """
         if cls.__name__ == "Rectangle":
             new = cls(1, 1)
-        if cls.__name__ == "Square":
+            new.update(**dictionary)
+        elif cls.__name__ == "Square":
             new = cls(1)
-        new.update(**dictionary)
+            new.update(**dictionary)
         return new
 
     @classmethod
