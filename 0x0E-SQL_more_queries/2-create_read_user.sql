7337073eb9b6db9665c2785062b89cfcf148f9e8
Lisa Olson <olson.lisa94@gmail.com>
2018-06-26 23:22:20 +0000

    grants usage access

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,6 @@
 -- Creates the database hbtn_0d_2 and adds user
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+GRANT USAGE ON *.* TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON *.* TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 FLUSH PRIVILEGES;
