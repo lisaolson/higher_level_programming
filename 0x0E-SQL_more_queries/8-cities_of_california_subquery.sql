3c366dc10063cd5c75c3b64fa0a443952ec37dac
Lisa Olson <olson.lisa94@gmail.com>
2018-06-27 03:32:01 +0000

    lists al cities found in hbtn_0d_usa

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
--- Lists all the cities of California that can be found in the database
-USE hbtn_0d_usa;
-SELECT id, name FROM cities
+-- Lists all the cities of California that can be found in database hbtn_0d_usa
+SELECT id, name
+FROM cities
 WHERE state_id =
 	(SELECT id
 	FROM states
