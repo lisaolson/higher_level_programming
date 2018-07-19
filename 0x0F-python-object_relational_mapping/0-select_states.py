05541543e608ca91fe358cb747c1496edbcf3ffd
Lisa Olson <olson.lisa94@gmail.com>
2018-07-19 17:20:12 +0000

    lists all states from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+
+db = MySQLdb.connect(
+	host="localhost",
+	user=sys.argv[1],
+	passwd=sys.argv[2],
+	db=sys.argv[3],
+	port=3306
+)
+
+cur = db.cursor()
+cur.execute("SELECT * FROM states ORDER BY id ASC")
+query_rows = cur.fetchall()
+for row in query_rows:
+	print(row)
+cur.close()
+db.close()
