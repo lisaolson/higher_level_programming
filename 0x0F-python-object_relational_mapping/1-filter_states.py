865a774af5e7820154eba1025c7f042347f8cc9f
Lisa Olson <olson.lisa94@gmail.com>
2018-07-19 17:30:15 +0000

    lists all states with a name starting with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module to list all states from the database
+"""
+import MySQLdb
+import sys
+
+
+db = MySQLdb.connect(
+	host="localhost",
+	user=sys.argv[1],
+	passwd=sys.argv[2],
+	db=sys.argv[3],
+	port=3306
+)
+
+cur = db.cursor()
+cur.execute("SELECT * FROM states WHERE Name REGEXP '^[N]' ORDER BY id ASC")
+query_rows = cur.fetchall()
+for row in query_rows:
+	print(row)
+cur.close()
+db.close()
