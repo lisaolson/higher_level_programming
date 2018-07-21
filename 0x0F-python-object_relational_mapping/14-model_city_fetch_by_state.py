003a4a2df1e9c7d7bc8e6bbbb0fb46dfde5162a6
Lisa Olson <olson.lisa94@gmail.com>
2018-07-21 01:55:19 +0000

    indentation

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -25,7 +25,8 @@ if __name__ == "__main__":
 
     session = Session()
 
-    for state, city in session.query(State, City).filter(State.id == City.state_id).all():
+    for state, city in session.query(State, City)\
+            .filter(State.id == City.state_id).all():
         print("{}: ({}) {}".format(state.name, city.id, city.name))
 
     session.close()
