897d10b3ce71d3e0126dabe30d6053dc03f257fc
Lisa Olson <olson.lisa94@gmail.com>
2018-08-06 22:17:26 +0000

    Combines print statement into one

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -8,8 +8,6 @@ import sys
 if __name__ == '__main__':
     try:
         with urllib.request.urlopen(sys.argv[1]) as response:
-            html = response.read()
-            utf8 = html.decode('utf-8')
-        print(html.decode('utf-8'))
+            print(response.read().decode('utf-8'))
     except urllib.error.URLError as e:
         print("Error code: {}".format(e.reason))
