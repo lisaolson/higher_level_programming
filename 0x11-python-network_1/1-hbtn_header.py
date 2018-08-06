1c338e69b0e56e9f8c511861e0c0629827acabd0
Lisa Olson <olson.lisa94@gmail.com>
2018-08-06 21:52:32 +0000

    returns X-Request-Id value

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Takes in a URL, sends a request, displays X-Request-Id value
+
+import urllib.request
+import sys
+
+if __name__ == '__main__':
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        html = response.info()
+
+    print(html.get("X-Request-Id"))
