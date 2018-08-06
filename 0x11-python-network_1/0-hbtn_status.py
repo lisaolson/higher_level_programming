c2e2531df84e2ff758e8c67b71df313ed9c07b8a
Lisa Olson <olson.lisa94@gmail.com>
2018-08-06 21:18:09 +0000

    corrects tabbing

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,6 +9,6 @@ if __name__ == '__main__':
         utf8 = html.decode('utf-8')
 
     print("Body response:")
-    print("\t - type: {}".format(type(html)))
-    print("\t - content: {}".format(html))
-    print("\t - utf8 content: {}".format(utf8))
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(utf8))
