747f6fefbb7c74648247f40a1d578d8429210aa5
Lisa Olson <olson.lisa94@gmail.com>
2018-08-06 22:08:13 +0000

    takes in URL and email, sends post request

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# Takes in URL and email, sends POST request
+
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    values = {'email':sys.argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('utf-8')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        html = response.read().decode('utf-8')
+
+    print("{}".format(html))
