870ed0b3ab5d9074c27102b48d8fd4c5489a246b
Lisa Olson <olson.lisa94@gmail.com>
2018-08-28 23:05:37 +0000

    places let within loop

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 let x = process.argv[2];
-let i = 0;
 if (isNaN(x)) {
   console.log('Missing number of occurrences');
 }
-for(i = 0; i < x; i++) {
+for (let i = 0; i < x; i++) {
   console.log('C is fun');
 }
