748e82cc1db24109918b6ba1431333271b11bf06
Lisa Olson <olson.lisa94@gmail.com>
2018-08-28 23:00:00 +0000

    corrects formatting

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+if (typeof(process.argv[2]) === undefined) {
+  console.log('Not a number');
+} else {
+  if (parseInt(process.argv[2] === 'NaN') {
+    console.log('Not a number');
+  } else {
+    console.log('My number: ' + parseInt(process.argv[2], 10));
+  }
+}
