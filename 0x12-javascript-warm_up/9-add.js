f351ea42744ab5fcd58de0d3bc5342ca3e028781
Lisa Olson <olson.lisa94@gmail.com>
2018-08-28 23:08:36 +0000

    removes let

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-let first = Number(process.argv[2]);
-let second = Number(process.argv[3]);
-function add(a, b) {
+function add (a, b) {
   return (a + b);
 }
-console.log(add(first, second));
+console.log(add(Number(process.argv[2]), Number(process.argv[3])));
