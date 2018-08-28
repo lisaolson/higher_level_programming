8db9b8c86300998cc1555ce9755c82d781e34dd0
Lisa Olson <olson.lisa94@gmail.com>
2018-08-28 23:04:52 +0000

    rearranges let declarations

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
 let x = process.argv[2];
-let i = 0;
-let j = 0;
-let square = '';
 if (isNaN(x)) {
   console.log('Missing size');
 }
-for(i = 0; i < x; i++) {
-  square = "";
-  for(j = 0; j < x; j++) {
+for (let i = 0; i < x; i++) {
+  let square = '';
+  for (let j = 0; j < x; j++) {
     square += 'X';
   }
   console.log(square);
