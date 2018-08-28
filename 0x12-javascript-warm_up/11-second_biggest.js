e953982b2ac82fbe83867a80ac3926a9830f910a
Lisa Olson <olson.lisa94@gmail.com>
2018-08-28 23:58:47 +0000

    corrects logic for no args

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
-if (process.argv.length === 1) {
-  console.log("0");
-}
 if (process.argv.length === 2) {
-  console.log("0");
+  console.log('0');
+} else if (process.argv.length === 3) {
+  console.log('0');
+} else {
+  let arr = [];
+  for (let i = 2; i < process.argv.length; i++) {
+    arr.push(Number(process.argv[i]));
+  }
+  arr = arr.sort();
+  console.log(arr[arr.length - 2]);
 }
-let arr = [];
-for(let i = 2; i < process.argv.length; i++) {
-  arr.push(Number(process.argv[i]));
-}
-arr = arr.sort()
-console.log(arr[arr.length - 2]);
