29d630bbc033fefbc7829f0cdfeeeaf09094f3e3
Lisa Olson <olson.lisa94@gmail.com>
2018-08-28 23:31:18 +0000

    corrects logic for checking args

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
-if (typeof(process.argv[2]) === undefined) {
+if (isNaN(process.argv[2])) {
   console.log('Not a number');
 } else {
-  if (parseInt(process.argv[2] === 'NaN') {
-    console.log('Not a number');
-  } else {
-    console.log('My number: ' + parseInt(process.argv[2], 10));
-  }
+  console.log('My number: ' + parseInt(process.argv[2], 10));
 }
