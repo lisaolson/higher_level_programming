addfc9ff29b7f401e7f1cc1bce3b15360cd455a5
Lisa Olson <olson.lisa94@gmail.com>
2018-09-04 22:40:25 +0000

    completes Square class

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let square = '';
+      for (let j = 0; j < this.width; j++) {
+        square += 'X';
+      }
+      console.log(square);
+    }
+  }
+  rotate () {
+    let temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+
+module.exports = Square;
