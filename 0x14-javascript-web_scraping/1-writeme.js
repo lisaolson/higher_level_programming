bd2baef27535a3115c3ea2408d5e4e174a20d25a
Lisa Olson <olson.lisa94@gmail.com>
2018-09-05 18:21:15 +0000

    corrects formatting

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
+// Writes a string to a file
 
 let fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], 'utf8', function(err) {
-    if(err) {
-        return console.log(err);
-    }
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err) {
+  if (err) {
+    return console.log(err);
+  }
 });
