86b94e9d37872bc0c273ec452947d89ec598aeac
Lisa Olson <olson.lisa94@gmail.com>
2018-09-05 21:55:10 +0000

    prints the number of movies where a certain character is present

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+// Print the number of movies where the character "Wedge Antilles" is present
+
+const request = require('request');
+let films = '';
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  films = JSON.parse(body).results;
+  let count = 0;
+  for (let i = 0; i < films.length; i++) {
+    for (let j = 0; j < films[i].characters.length; j++) {
+      if (films[i].characters[j] === 'https://swapi.co/api/people/18/') {
+        count += 1;
+      }
+    }
+  }
+  console.log(count);
+});
