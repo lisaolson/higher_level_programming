9f48c0a3e4130a4f8b3e0dccfc22d5f0c455ad7e
Lisa Olson <olson.lisa94@gmail.com>
2018-09-05 19:16:26 +0000

    corrects formatting

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,6 +5,6 @@ let fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', function (err, data) {
   if (err) {
     return console.log(err);
-    }
-      console.log(data);
+  }
+  console.log(data);
 });
