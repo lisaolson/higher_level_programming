d5dcdf0cc29e64d3fcc843119c00e32741e34f3d
Lisa Olson <olson.lisa94@gmail.com>
2018-09-05 19:16:07 +0000

    returns log message

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,9 @@
 
 let fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf8', function(err, data) {
-    if (err) console.log(err);
-    console.log(data);
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (err) {
+    return console.log(err);
+    }
+      console.log(data);
 });
