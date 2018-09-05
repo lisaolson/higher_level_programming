1d25fdd337b22494aaf58b327700105385a9c0d8
Lisa Olson <olson.lisa94@gmail.com>
2018-09-05 20:45:02 +0000

    error handling

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,8 @@
 
 let request = require('request');
 request(process.argv[2], function (error, response) {
+  if (error) {
+    console.log(error);
+  }
   console.log('code:', response.statusCode);
 });
