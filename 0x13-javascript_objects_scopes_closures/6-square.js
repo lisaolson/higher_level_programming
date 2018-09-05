702bc978c8f0900d1dcae292df68cfcefa349449
Lisa Olson <olson.lisa94@gmail.com>
2018-09-05 05:46:15 +0000

    corrects the class inheritance

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,37 +1,6 @@
 #!/usr/bin/node
 
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      let square = '';
-      for (let j = 0; j < this.width; j++) {
-        square += 'X';
-      }
-      console.log(square);
-    }
-  }
-  rotate () {
-    let temp = this.height;
-    this.height = this.width;
-    this.width = temp;
-  }
-  double () {
-    this.height = this.height * 2;
-    this.width = this.width * 2;
-  }
-}
-
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+class Square extends require('./5-square') {
   charPrint (c) {
     if (c === undefined) {
       for (let i = 0; i < this.height; i++) {
@@ -51,6 +20,6 @@ class Square extends Rectangle {
       }
     }
   }
-}
+};
 
 module.exports = Square;
