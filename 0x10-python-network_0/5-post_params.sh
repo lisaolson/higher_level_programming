1b0fae1447dd23d93fd64a1e6633298111cc50df
Lisa <olson.lisa94@gmail.com>
2020-07-24 17:55:02 -0400

    corrects all files

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL, sends POST request to passed URL, displays body
-curl -sL $1 -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+curl -sL $1 -X POST -d "email=hr@closchool.com&subject=I will always be here for PLD"
